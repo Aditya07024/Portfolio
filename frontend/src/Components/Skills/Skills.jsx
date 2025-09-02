def31fa32655414e8f38e5088accf5c8a29b280a
Aditya07024 <adityakumar07024@gmail.com>
2025-09-02 21:25:01 +0530

    add names to the skills and added new skills

--- frontend/src/Components/Skills/Skills.jsx
+++ frontend/src/Components/Skills/Skills.jsx
@@ -15,9 +15,11 @@ import postman from "../../assets/postman.png";
 import python from "../../assets/python.png";
 import java from "../../assets/java.png";
 import aws from "../../assets/aws.png";
-import socket from "../../assets/Socket-io.svg"
-import websockets from "../../assets/images.png"
-import jwt from "../../assets/jwt-flower.webp"
+import socket from "../../assets/Socket-io.svg";
+import websockets from "../../assets/images.png";
+import jwt from "../../assets/jwt-flower.webp";
+import typescript from "../../assets/typescript.svg";
+import nextjs from "../../assets/nextjs.jpeg";
 const Skills = () => {
   return (
     <div className="skills-div" id="skills">
@@ -27,85 +29,91 @@ const Skills = () => {
       <div className="main-sec">
         <div className="skills">
           <div className="img-container">
-            <img src={react}></img>
-            <p>React</p>
+            <img src={react} alt="ReactJS" />
+            <p>ReactJS</p>
           </div>
           <div className="img-container">
-            <img src={reactnative}></img>
-            <p>React Native</p>
+            <img src={nextjs} alt="NextJS" />
+            <p>NextJS</p>
           </div>
           <div className="img-container">
-            <img src={tailwindcss}></img>
+            <img src={tailwindcss} alt="Tailwind CSS" />
             <p>Tailwind CSS</p>
           </div>
           <div className="img-container">
-            <img src={css}></img>
+            <img src={css} alt="CSS" />
             <p>CSS</p>
           </div>
-          
           <div className="img-container">
-            <img src={figma}></img>
+            <img src={figma} alt="Figma" />
             <p>Figma</p>
           </div>
         </div>
         <div className="skills">
           <div className="img-container">
-            <img src={nodejs}></img>
+            <img src={nodejs} alt="Node.js" />
             <p>Node.js</p>
           </div>
           <div className="img-container">
-            <img src={expressjs}></img>
+            <img src={expressjs} alt="Express" />
             <p>Express</p>
           </div>
           <div className="img-container">
-            <img src={javascript}></img>
+            <img src={javascript} alt="JavaScript" />
             <p>JavaScript</p>
           </div>
           <div className="img-container">
-            <img src={mongodb}></img>
-            <p>MongoDB</p>
+            <img src={typescript} alt="TypeScript" />
+            <p>TypeScript</p>
           </div>
           <div className="img-container">
-            <img src={sql}></img>
-            <p>SQL</p>
-          </div>
-        </div>
-        <div className="skills">
-          <div className="img-container">
-            <img src={firebase}></img>
-            <p>Firebase</p>
-          </div>
-          <div className="img-container">
-            <img src={postman}></img>
-            <p>Postman</p>
-          </div>
-          <div className="img-container">
-            <img src={aws}></img>
-            <p>AWS</p>
-          </div>
-          <div className="img-container">
-            <img src={java}></img>
-            <p>Java</p>
-          </div>
-          <div className="img-container">
-            <img src={python}></img>
+            <img src={python} alt="Python" />
             <p>Python</p>
           </div>
         </div>
         <div className="skills">
           <div className="img-container">
-            <img src={socket}></img>
+            <img src={firebase} alt="Firebase" />
+            <p>Firebase</p>
+          </div>
+          <div className="img-container">
+            <img src={postman} alt="Postman" />
+            <p>Postman</p>
+          </div>
+          <div className="img-container">
+            <img src={aws} alt="AWS" />
+            <p>AWS</p>
+          </div>
+          <div className="img-container">
+            <img src={mongodb} alt="MongoDB" />
+            <p>MongoDB</p>
+          </div>
+          <div className="img-container">
+            <img src={sql} alt="SQL" />
+            <p>SQL</p>
+          </div>
+        </div>
+        <div className="skills">
+          <div className="img-container">
+            <img src={socket} alt="Socket.io" />
             <p>Socket.io</p>
           </div>
           <div className="img-container">
-            <img src={websockets} style={{borderRadius:"50%"}}></img>
-            <p>WebSockets</p>
+            <img
+              src={websockets}
+              style={{ borderRadius: "50%" }}
+              alt="Websockets"
+            />
+            <p>Websockets</p>
           </div>
           <div className="img-container">
-            <img src={jwt}></img>
+            <img src={jwt} alt="JWT" />
             <p>JWT</p>
           </div>
-          
+          <div className="img-container">
+            <img src={reactnative}></img>
+            <p>ReactNative</p>
+          </div>
         </div>
       </div>
     </div>
